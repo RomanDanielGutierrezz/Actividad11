988365c9502fb3e231beec7c349cfa8a36eba481
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-06-06 10:41:05 -0300

    EJ4

---
+++ EJ4/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace EJ4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtValores = new TextBox();
+            btoResult = new Button();
+            lbPromedio = new Label();
+            lbRegistro = new Label();
+            btoRegistrar = new Button();
+            txtRegistro = new TextBox();
+            btoOrdenar = new Button();
+            SuspendLayout();
+            // 
+            // txtValores
+            // 
+            txtValores.Location = new Point(12, 195);
+            txtValores.Multiline = true;
+            txtValores.Name = "txtValores";
+            txtValores.Size = new Size(316, 128);
+            txtValores.TabIndex = 17;
+            // 
+            // btoResult
+            // 
+            btoResult.Location = new Point(314, 100);
+            btoResult.Name = "btoResult";
+            btoResult.Size = new Size(122, 68);
+            btoResult.TabIndex = 16;
+            btoResult.Text = "Calcular Promedio";
+            btoResult.UseVisualStyleBackColor = true;
+            btoResult.Click += btoResult_Click;
+            // 
+            // lbPromedio
+            // 
+            lbPromedio.AutoSize = true;
+            lbPromedio.Location = new Point(125, 127);
+            lbPromedio.Name = "lbPromedio";
+            lbPromedio.Size = new Size(69, 15);
+            lbPromedio.TabIndex = 15;
+            lbPromedio.Text = "lbPromedio";
+            // 
+            // lbRegistro
+            // 
+            lbRegistro.AutoSize = true;
+            lbRegistro.Location = new Point(60, 39);
+            lbRegistro.Name = "lbRegistro";
+            lbRegistro.Size = new Size(83, 15);
+            lbRegistro.TabIndex = 14;
+            lbRegistro.Text = "Indicar Valores";
+            // 
+            // btoRegistrar
+            // 
+            btoRegistrar.Location = new Point(314, 12);
+            btoRegistrar.Name = "btoRegistrar";
+            btoRegistrar.Size = new Size(122, 68);
+            btoRegistrar.TabIndex = 13;
+            btoRegistrar.Text = "Registrar";
+            btoRegistrar.UseVisualStyleBackColor = true;
+            btoRegistrar.Click += btoRegistrar_Click;
+            // 
+            // txtRegistro
+            // 
+            txtRegistro.Location = new Point(149, 36);
+            txtRegistro.Name = "txtRegistro";
+            txtRegistro.Size = new Size(122, 23);
+            txtRegistro.TabIndex = 12;
+            // 
+            // btoOrdenar
+            // 
+            btoOrdenar.Location = new Point(361, 195);
+            btoOrdenar.Name = "btoOrdenar";
+            btoOrdenar.Size = new Size(75, 79);
+            btoOrdenar.TabIndex = 18;
+            btoOrdenar.Text = "Ordenar";
+            btoOrdenar.UseVisualStyleBackColor = true;
+            btoOrdenar.Click += btoOrdenar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(444, 331);
+            Controls.Add(btoOrdenar);
+            Controls.Add(txtValores);
+            Controls.Add(btoResult);
+            Controls.Add(lbPromedio);
+            Controls.Add(lbRegistro);
+            Controls.Add(btoRegistrar);
+            Controls.Add(txtRegistro);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtValores;
+        private Button btoResult;
+        private Label lbPromedio;
+        private Label lbRegistro;
+        private Button btoRegistrar;
+        private TextBox txtRegistro;
+        private Button btoOrdenar;
+    }
+}
--- EJ4/Form1.cs
+++ EJ4/Form1.cs
@@ -1,4 +1,4 @@
-namespace EJ3
+namespace EJ4
 {
     public partial class Form1 : Form
     {
@@ -7,6 +7,11 @@ namespace EJ3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
         private void btoRegistrar_Click(object sender, EventArgs e)
         {
@@ -22,13 +27,30 @@ namespace EJ3
                 acumulador += valores[i];
             }
             double promedio = acumulador / contador;
-            lbPromedio.Text = $"El promedio es: {promedio}";
+            lbPromedio.Text = $"El promedio es: {promedio:f2}";
             txtValores.Text = $@"El promedio es: 
                               {promedio,6:f2}";
         }
-        private void txtRegistro_TextChanged(object sender, EventArgs e)
-        {
 
+        private void btoOrdenar_Click(object sender, EventArgs e)
+        {
+            txtValores.Clear();
+            for (int i = 0; i < contador; i++)
+            {
+                txtValores.Text += $"{valores[i],5:f2}";
+            }
+            for (int i = 0; i < contador-1; i++)
+            {
+                for (int j = i+1; j < contador; j++)
+                {
+                    if (valores[i] > valores[j])
+                    {   
+                        double aux = valores[i];
+                        valores[i] = valores[j];
+                        valores[j] = aux;
+                    }
+                }
+            }
         }
     }
 }
