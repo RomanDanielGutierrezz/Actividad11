2bbba058fb1457c08c623e9b27d7b2f0be341415
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-06-06 09:54:35 -0300

    EJ3

--- EJ3/Form1.Designer.cs
+++ EJ3/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtValores = new TextBox();
+            btoResult = new Button();
+            lbPromedio = new Label();
+            lbRegistro = new Label();
+            btoRegistrar = new Button();
+            txtRegistro = new TextBox();
+            SuspendLayout();
+            // 
+            // txtValores
+            // 
+            txtValores.Location = new Point(56, 216);
+            txtValores.Multiline = true;
+            txtValores.Name = "txtValores";
+            txtValores.Size = new Size(316, 128);
+            txtValores.TabIndex = 11;
+            // 
+            // btoResult
+            // 
+            btoResult.Location = new Point(265, 131);
+            btoResult.Name = "btoResult";
+            btoResult.Size = new Size(122, 68);
+            btoResult.TabIndex = 10;
+            btoResult.Text = "Calcular Promedio";
+            btoResult.UseVisualStyleBackColor = true;
+            // 
+            // lbPromedio
+            // 
+            lbPromedio.AutoSize = true;
+            lbPromedio.Location = new Point(80, 158);
+            lbPromedio.Name = "lbPromedio";
+            lbPromedio.Size = new Size(69, 15);
+            lbPromedio.TabIndex = 9;
+            lbPromedio.Text = "lbPromedio";
+            // 
+            // lbRegistro
+            // 
+            lbRegistro.AutoSize = true;
+            lbRegistro.Location = new Point(25, 44);
+            lbRegistro.Name = "lbRegistro";
+            lbRegistro.Size = new Size(83, 15);
+            lbRegistro.TabIndex = 8;
+            lbRegistro.Text = "Indicar Valores";
+            // 
+            // btoRegistrar
+            // 
+            btoRegistrar.Location = new Point(265, 17);
+            btoRegistrar.Name = "btoRegistrar";
+            btoRegistrar.Size = new Size(122, 68);
+            btoRegistrar.TabIndex = 7;
+            btoRegistrar.Text = "Registrar";
+            btoRegistrar.UseVisualStyleBackColor = true;
+            btoRegistrar.Click += btoRegistrar_Click;
+            // 
+            // txtRegistro
+            // 
+            txtRegistro.Location = new Point(114, 41);
+            txtRegistro.Name = "txtRegistro";
+            txtRegistro.Size = new Size(122, 23);
+            txtRegistro.TabIndex = 6;
+            txtRegistro.TextChanged += txtRegistro_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(424, 366);
+            Controls.Add(txtValores);
+            Controls.Add(btoResult);
+            Controls.Add(lbPromedio);
+            Controls.Add(lbRegistro);
+            Controls.Add(btoRegistrar);
+            Controls.Add(txtRegistro);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtValores;
+        private Button btoResult;
+        private Label lbPromedio;
+        private Label lbRegistro;
+        private Button btoRegistrar;
+        private TextBox txtRegistro;
     }
 }
--- EJ3/Form1.cs
+++ EJ3/Form1.cs
@@ -2,9 +2,34 @@ namespace EJ3
 {
     public partial class Form1 : Form
     {
+        int contador = 0;
+        double[] valores = new double[100];
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btoRegistrar_Click(object sender, EventArgs e)
+        {
+            valores[contador] = Convert.ToDouble(txtRegistro.Text);
+            contador++;
+            txtRegistro.Clear();
+        }
+        private void btoResult_Click(object sender, EventArgs e)
+        {
+            double acumulador = 0;
+            for (int i = 0; i < contador; i++)
+            {
+                acumulador += valores[i];
+            }
+            double promedio = acumulador / contador;
+            lbPromedio.Text = $"El promedio es: {promedio}";
+            txtValores.Text = $@"El promedio es: 
+                              {promedio,6:f2}";
+        }
+
+        private void txtRegistro_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
